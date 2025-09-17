3d23d3d5164258f3f1b3e96ffc8be10862c37f1f
Amit08510 <actionchaudhari9090@gmail.com>
2025-09-17 18:22:31 +0545

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,14 @@
+// <?php
+// $servername = "localhost";
+// $username = "root";    
+// $password = "";  
+// $database = "uk_esports"; 
+
+// ?>
+
 <?php
-$servername = "localhost";
-$username = "root";    
-$password = "";  
-$database = "uk_esports"; 
+$servername = "sql106.infinityfree.com";
+$username = "if0_39941198";    
+$password = "b220ODmoDLHvi8A";  
+$database = "if0_39941198_esports"; 
 ?>
